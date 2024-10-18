15f3f102b913a782cda234a132fe0678342ee3e5
HongGunWoo <foxrain.gg@gmail.com>
2024-10-18 10:58:12 +0900

    feat: note page 추가

---
+++ src/app/note/page.tsx
@@ -0,0 +1,63 @@
+import { allNotes } from 'contentlayer/generated';
+import type { Note } from '.contentlayer/generated/types';
+import { compareDesc, format } from 'date-fns';
+
+export default function Note() {
+  const notes = allNotes.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date)),
+  );
+  const yearByNotes: Map<number, Note[]> = notes.reduce((acc, note) => {
+    const year = new Date(note.date).getFullYear();
+
+    if (!acc.get(year)) {
+      acc.set(year, []);
+    }
+
+    acc.get(year).push(note);
+
+    return acc;
+  }, new Map());
+
+  return (
+    <div className="group flex h-full w-full flex-col">
+      {Array.from(yearByNotes.entries()).map(([year, notes], idx) => {
+        return (
+          <div key={year} className="group/year relative flex">
+            <p
+              className={`${
+                idx !== 0 ? 'mt-8' : ''
+              } mr-5 w-12 text-base font-medium transition group-hover/year:!opacity-100 group-hover:opacity-60`}
+            >
+              {year}
+            </p>
+            <ul
+              className={`${
+                idx !== 0 ? 'pt-8' : ''
+              } flex flex-col gap-3 border-l border-gray-200 pl-4 group-hover/year:!opacity-100 group-hover:opacity-60`}
+            >
+              {notes.map((note) => (
+                <li
+                  key={note._raw.flattenedPath}
+                  className="flex cursor-pointer items-center rounded-md px-1 transition hover:bg-gray-100 hover:!opacity-100 group-hover:opacity-60"
+                >
+                  <h3>{note.title}</h3>
+                  <time
+                    dateTime={note.date}
+                    className="ml-2 block text-sm text-gray-400"
+                  >
+                    {format(new Date(note.date), 'MM.dd')}
+                  </time>
+                </li>
+              ))}
+            </ul>
+            <div
+              className={`absolute left-14 h-3 w-3 translate-x-1/2 rounded-full bg-primary ${
+                idx !== 0 ? 'top-8' : ''
+              } group-hover/year:!opacity-100 group-hover:opacity-60`}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/constants/headerNavLinks.ts
+++ src/constants/headerNavLinks.ts
@@ -1,10 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/post', title: 'Posts' },
-  // {
-  //   href: 'https://cooing-beaufort-e0f.notion.site/bde4ddbd9dba4fd28f4816ceed1de2c3',
-  //   title: 'Portfolio',
-  // },
+  { href: '/post', title: 'Post' },
+  { href: '/note', title: 'Note' },
   { href: '/about', title: 'About' },
 ];
 
